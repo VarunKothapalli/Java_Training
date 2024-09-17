dba24a0f8dd9ec0266a5a3bee9a3f11c58ab1df0
VarunKothapalli <kvsvb1995@gmail.com>
2024-09-17 12:23:22 -0400

    FourFiveDays Practice

--- src/FourFiveDays/GreetUser.java
+++ src/FourFiveDays/GreetUser.java
@@ -1,4 +1,14 @@
 package FourFiveDays;
+import java.util.*;
 
 public class GreetUser {
+
+    public static void  main (String [] args) {
+
+        greetUser("Varun");
+    }
+
+    public static void greetUser (String name) {
+        System.out.println("Hello, " + name + "! Welcome to our program.");
+    }
 }
