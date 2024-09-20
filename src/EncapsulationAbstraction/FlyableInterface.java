8dc8c1b2ddc52779fac7480715cb69e2f465f0ec
VarunKothapalli <kvsvb1995@gmail.com>
2024-09-20 12:30:23 -0400

    Encapsulation & Polymorphism

--- src/EncapsulationAbstraction/FlyableInterface.java
+++ src/EncapsulationAbstraction/FlyableInterface.java
@@ -5,6 +5,8 @@ public class FlyableInterface {
 
     public static void main (String [] args) {
 
+        Flyable f1 = new Airplane();
+        f1.flyable();
     }
 }
 
@@ -12,6 +14,10 @@ public class FlyableInterface {
 interface Flyable {
 
     public void fly();
+
+    default void flyable () {
+        System.out.println("flyable");
+    }
 }
 
 class Bird implements Flyable {
